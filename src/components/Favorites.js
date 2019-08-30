ca2e6239caf5754839883c08b3e3fa50a1947afd
MrNivorous <karn.sood@gmail.com>
2019-08-30 13:19:18 +0700

    060302

---
+++ src/components/Favorites.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Favorites.css'
+
+class Favorites extends React.Component {
+
+render () {
+	return (
+		<div className="favorites">Favorites</div>
+	)
+}
+
+}
