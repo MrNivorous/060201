4d5586a12382b2df01f9ab7b9934a2e8c36a7472
MrNivorous <karn.sood@gmail.com>
2019-09-01 15:17:39 +0700

    060502

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -7,8 +7,8 @@ state = {
 	active: false
 }
 
-toggleClass() {
-		 let currentState = this.state.active
+toggleClass () {
+	let currentState = this.state.active
 		 this.setState({ active: !currentState })
  }
 
@@ -18,7 +18,7 @@ render () {
 					<p><h3>{this.props.place.title}</h3></p>
 					<p>${this.props.place.price}</p>
 					<p>{this.props.place.location}</p>
-					<button className="like" onClick={() => this.toggleClass}>Like</button>
+					<button className={this.state.active ? "liked": "like"} onClick={() => this.toggleClass()} >Like</button>
 			</div>
 	)
 }
